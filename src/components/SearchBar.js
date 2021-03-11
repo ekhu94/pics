8eb38b5428d4d0ba1e56fab83d51f765392faf57
Erik Huang <erikhuangdrums@gmail.com>
2021-03-10 20:01:30 -0800

    prevent default on form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,20 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    // onFormSubmit(e) {
-    //     e.preventDefault();
-    //     const input = document.querySelector('input');
-    //     console.log(`submitted: ${input.value}`)
-    // }
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
                         <input 
                             type="text" 
-                            value={this.state.term} 
-                            onChange={e => this.setState({ term: e.target.value })} 
+                            value={ this.state.term }
+                            onChange={ e => this.setState({ term: e.target.value }) }
                         />
                     </div>
                 </form>
