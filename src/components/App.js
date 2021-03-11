cfd5bdcd14a164b9e085238e55deb4fe089d3a13
Erik Huang <erikhuangdrums@gmail.com>
2021-03-11 06:41:19 -0800

    refactor async func as arrow w/ state

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    state = { images: [] };
 
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    onSearchSubmit = async term => {
+        const res = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID F10iZjBy5js5EuZGx9n0dTbBzU8eBsOP7o7lJ-cq91U'
             }
         });
+        this.setState({ images: res.data.results });
+        console.log(this.state.images);
     }
 
     render() {
