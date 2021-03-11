882248edc1556b55d8fdc777ffe7b2f46491877e
Erik Huang <erikhuangdrums@gmail.com>
2021-03-11 10:53:03 -0800

    create css file for ImageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = props => {
     const imgs = props.images.map(({ id, alt_description, urls }) => {
-        return <img key={id} style={{width: '200px', margin: '10px'}} src={urls.regular} alt={alt_description} />
+        return <img key={id} src={urls.regular} alt={alt_description} />
     });
-    return <div>{imgs}</div>
+    return <div className="image-list">{imgs}</div>
 }
 
 export default ImageList;
