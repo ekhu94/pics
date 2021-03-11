d40203e2f9cf5ec5d3c1adbec82cee9b4f3a23df
Erik Huang <erikhuangdrums@gmail.com>
2021-03-10 19:26:44 -0800

    create event for onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(e) {
+        console.log(e.target.value);
+    }
+
+    // onFormSubmit(e) {
+    //     e.preventDefault();
+    //     const input = document.querySelector('input');
+    //     console.log(`submitted: ${input.value}`)
+    // }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
-                        <label for="search">Image Search</label>
-                        <input id="search" type="text" />
+                        <label>Image Search</label>
+                        <input type="text" onChange={ this.onInputChange } />
                     </div>
                 </form>
             </div>
