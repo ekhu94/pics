7e42f7f2263d08009597577b6d2c22c35544b550
Erik Huang <erikhuangdrums@gmail.com>
2021-03-11 19:03:05 -0800

    remove all files for review

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID F10iZjBy5js5EuZGx9n0dTbBzU8eBsOP7o7lJ-cq91U'
-    }
-});
